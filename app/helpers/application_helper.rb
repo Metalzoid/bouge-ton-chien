481c4635fc9b4875e410db760c0c1b1d636837f1
Audrey JEGOU <audrey.jegou@gmail.com>
2024-06-06 18:08:52 +0200

    save

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,8 @@ module ApplicationHelper
   def logodoggies_class(position)
     klass = "logodoggies"
     klass += "-final" if position == 4
-    klass += " reached" if current_user.money >= 5*position
-    klass += " middle" if position > 0 && current_user.money >= 5*position
+    klass += " reached" if current_user.money >= 50*position
+    klass += " middle" if position > 0 && current_user.money >= 50*position
     return klass
   end
 end
