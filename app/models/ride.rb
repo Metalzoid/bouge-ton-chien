98c5ef3738ce7b1996ba280a617c5c66cf8378dc
Florian Gagnaire <gagnaire.flo@gmail.com>
2024-06-13 11:17:34 +0200

    done

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -7,4 +7,15 @@ class Ride < ApplicationRecord
     updated_at.month < 10 ? month = "0#{updated_at.month}" : month = updated_at.month
     return "#{day}/#{month}/#{updated_at.year}"
   end
+
+  def duration
+    if !timing.nil?
+      hours = timing / 3600
+      minutes = (timing % 3600) / 60
+      remaining_seconds = timing % 60
+      { hours: hours, minutes: minutes, seconds: remaining_seconds }
+    else
+      { hours: 0, minutes: 0, seconds: 0 }
+    end
+  end
 end
